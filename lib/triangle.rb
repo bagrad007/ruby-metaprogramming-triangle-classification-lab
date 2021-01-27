229f346871baa1f42a038084d7a131d8a5b99729
Luke Gill <bagrad007@gmail.com>
2021-01-27 13:18:00 -0500

    commit tree 935f534c1a6cd83b0b7301396f4ef090d7925910

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind 
+  
+    if a + b <= c || b + c <= a || a + c <= b || a <= 0 || b <= 0 || c <= 0
+      raise TriangleError
+    else
+      if a == b && b==c && a == c
+        return :equilateral
+      elsif  b == c || a == c || a == b
+        return :isosceles
+      elsif a != b && b != c && a != c
+        return :scalene
+      end
+    end
+  end
+
+  class TriangleError < StandardError
+    #some code
+  end
 end
